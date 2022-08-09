1267522cbf029e60aab24bb721a93d7503a4bbf8
Maria Mihu <mvmihu@gmail.com>
2022-08-09 11:15:24 -0400

    init, increment, and getScore

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,37 @@
-//! This contract implements simple counter backed by storage on blockchain.
-//!
-//! The contract provides methods to [increment] / [decrement] counter and
-//! get it's current value [getNum] or [reset].
-import { NearContract, NearBindgen, near, call, view } from 'near-sdk-js'
+import { NearContract, NearBindgen, near, call, view } from "near-sdk-js"
+
+const SCORE_THRESHOLD = 10;
 
 @NearBindgen
-class Counter extends NearContract {
-    val: number;
-    constructor() {
+class ScavengerHunt extends NearContract {
+    score: number;
+    accountOwner: string;
+    
+    constructor(accountOwner: string) {
         super()
-        this.val = 0
-    }
-
-    default() {
-        return new Counter()
+        this.accountOwner = accountOwner; //is there a way to assert this is valid
+        this.score = 0;
     }
 
     @call
-    /// Public method: Increment the counter.
     increment() {
-        this.val += 1;
-        near.log(`Increased number to ${this.val}`)
+        let prevScore = this.score;
+        this.score += 1;
+        near.log(`Congrats! ${this.accountOwner} has located one item. Incrementing score from ${prevScore} to ${this.score}`)
+        near.log(`One moment. Checking if ${this.accountOwner} is eligible for their rewards...`)
     }
 
     @call
-    /// Public method: Decrement the counter.
-    decrement() {
-        this.val -= 1;
-        near.log(`Decreased number to ${this.val}`)
-    }
-
-    @call
-    /// Public method - Reset to zero.
-    reset() {
-        this.val = 0;
-        near.log(`Reset counter to zero`)
+    checkPrizeEligibility(){
+        let eligibility = this.score > SCORE_THRESHOLD
+        near.log(`Is ${this.accountOwner} eligible for a prize? ${eligibility}`)
+        return eligibility
     }
 
     @view
-    /// Public method: Returns the counter value.
-    getNum(): number {
-        return this.val
+    getScore(){
+        return this.score
     }
-}
 
+    // METHOD HERE FOR PERFORMING CROSS CONTRACT CALL TO MINT AN NFT????
+}
